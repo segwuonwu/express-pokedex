1e0e94969e85c481d84e4c26f33415081473c11b
segwuonwu <segwuonwu@gmail.com>
2020-02-18 10:57:53 -0800

    working on final detail route

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../models');
+var axios = require('axios');
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
     // TODO: Get all records from the DB and render to view
     models.pokemon.findAll().then(function(pokemon) { // models.pokemon is the name of the table
             res.render('pokemon/index', { pokemons: pokemon });
+        }).catch(err => {
+            res.send("ERROR");
         })
         // res.send('Render a page of favorites here');
 });
@@ -14,7 +17,32 @@ router.get('/', function(req, res) {
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
     // TODO: Get form data and add a new record to DB
-    res.send(req.body);
+    models.pokemon.create({
+        name: req.body.name,
+        nickname: null,
+        type: null
+    }).then(function() {
+        res.redirect('/pokemon')
+    }).catch(err => {
+        res.send("ERROR");
+    })
 });
+
+//GET /pokemon/:id
+router.get('/:id', function(req, res) {
+
+
+    // query model for pokemon with given ID in pokemon db
+    var num = req.params.id;
+
+    models.pokemon.findOne({
+        where: {
+            id: num
+        }
+    }).then(function(pokemon) {
+        // get name of db queried pokemon
+        // pass to api call via 
+    })
+})
 
 module.exports = router;
