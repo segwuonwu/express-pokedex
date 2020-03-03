68d600582f1238f06b5e7f4a1241fc454b7d7cc6
segwuonwu <segwuonwu@gmail.com>
2020-03-02 22:21:11 -0800

    All route done except the get route to show info

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const ejsLayouts = require('express-ejs-layouts');
-const db = require('./models');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -21,50 +20,6 @@ app.get('/', function(req, res) {
     })
 });
 
-// Index - GET /pokemon
-app.get('/pokemon', (req, res) => {
-    db.pokemon.findAll().then(poke => {
-        console.log(poke)
-        res.json(poke);
-    }).catch(err => {
-        console.log(err);
-        res.send("Error");
-    })
-})
-
-// Create - POST /pokemon (redirect to /pokemon)
-app.post('/pokemon', (req, res) => {
-    db.pokemon.findOrCreate({
-        where: {
-            name: req.body.name
-        },
-        defaults: {
-            name: req.body.name,
-        }
-    }).then(([poke, created]) => {
-        if (created) {
-            console.log(`Created ${pokemon.name}`);
-            res.redirect(`/pokemon`); ///${pokemon.id}
-        } else {
-            console.log(`Found ${pokemon.name}`);
-        }
-        res.send("Pokemon was created successfully!!");
-    }).catch(err => {
-        console.log(err);
-
-    });
-
-})
-
-// db.pokemon.create({
-//     name: 'Pikachu'
-// }).then(function(poke) {
-//     console.log('Created: ', poke.name)
-// })
-
-// db.pokemon.findAll().then(function(poke) {
-//     console.log('Found: ', poke.name)
-// })
 
 // Imports all routes from the pokemon routes file
 app.use('/pokemon', require('./routes/pokemon'));
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,52 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../models');
 
-// GET /pokemon - return a page with favorited Pokemon
+//GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
-  // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+    db.pokemon.findAll().then(poke => {
+        res.render('result', { pokemon: poke });
+        //res.json(poke);
+    }).catch(err => {
+        console.log(err);
+        res.send("Error");
+    })
 });
 
-// POST /pokemon - receive the name of a pokemon and add it to the database
-router.post('/', function(req, res) {
-  // TODO: Get form data and add a new record to DB
-  res.send(req.body);
-});
+// Create - POST /pokemon (redirect to /pokemon)
+router.post('/', (req, res) => {
+    db.pokemon.findOrCreate({
+        where: {
+            name: req.body.name
+        }
+    }).then(([poke, created]) => {
+        if (created) {
+            console.log(`Created ${poke.name}`);
+            res.redirect(`/pokemon`);
+        } else {
+            console.log(`Found ${poke.name}`);
+            res.send(`${poke.name} has already been added`);
+        }
+    }).catch(err => {
+        console.log(err);
+    });
+})
+
+// Show - GET /pokemon/:id
+router.get('/:id', (req, res) => {
+    db.pokemon.findOne({
+        where: {
+            id: req.params.id
+        }
+    }).then(poke => {
+        console.log(`Found ${poke.id}`);
+        // res.send(`Found ${poke.name}`);
+        res.render('info', { poke: poke });
+
+    }).catch(err => {
+        console.log(err)
+        res.send("ERROR NO ID FOUND");
+    });
+})
 
 module.exports = router;
