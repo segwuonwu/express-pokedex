fc022a9075f9d85dc0b734f2a8c15e37eb7e9134
segwuonwu <segwuonwu@gmail.com>
2020-03-02 23:32:04 -0800

    added the info view and get route

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 const db = require('../models');
 
 //GET /pokemon - return a page with favorited Pokemon
@@ -32,21 +33,37 @@ router.post('/', (req, res) => {
     });
 })
 
-// Show - GET /pokemon/:id
-router.get('/:id', (req, res) => {
-    db.pokemon.findOne({
-        where: {
-            id: req.params.id
-        }
-    }).then(poke => {
-        console.log(`Found ${poke.id}`);
-        // res.send(`Found ${poke.name}`);
-        res.render('info', { poke: poke });
 
-    }).catch(err => {
-        console.log(err)
-        res.send("ERROR NO ID FOUND");
-    });
-})
+router.get('/:name', function(req, res) {
+    if (req.params && req.params.name) {
+        request('https://pokeapi.co/api/v2/pokemon/' + req.params.name, function(error, response, body) {
+            if (error || response.statusCode != 200) {
+                res.render('error');
+            } else {
+                var poke = JSON.parse(body);
+                res.render('info', { poke: poke });
+            }
+        });
+    } else {
+        res.render('error');
+    }
+});
+
+// Show - GET /pokemon/:id
+// router.get('/:name', (req, res) => {
+//     db.pokemon.findOne({
+//         where: {
+//             name: req.params.id
+//         }
+//     }).then(poke => {
+//         console.log(`Found ${poke.name}`);
+//         //res.send(`Found ${poke.name}`);
+//         res.render('info', { poke: poke });
+
+//     }).catch(err => {
+//         console.log(err)
+//         res.send("ERROR NO ID FOUND");
+//     });
+// })
 
 module.exports = router;
